acb0821b13f353387c097354bb1e12c3db7e6d55
danipv54 <dpvandroid54#gmail.com>
2022-01-16 21:14:37 -0400

    basic animations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController, GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+@ViewChild('box') box: ElementRef;
+  constructor(private gestureCtrl: GestureController, private animationCtrl: AnimationController) {}
 
+
+  ngAfterViewInit(): void {
+    const moveGesture=  this.gestureCtrl.create({
+    el:this.box.nativeElement,
+    gestureName:'move',
+    threshold:0,
+    onStart:ev=>{
+      console.log('Start');
+    },
+    onMove:ev=>{
+    const curretX= ev.deltaX;
+    const curretY= ev.deltaY;
+
+    this.box.nativeElement.style.transform= `translate(${curretX}px, ${curretY}px )`;
+     },
+    onEnd:ev=>{
+      console.log('end');
+    }
+    });
+   moveGesture.enable();
+  }
+  onAnimation(){
+  const simpleAnimation= this.animationCtrl.create('whatever')
+  .addElement(this.box.nativeElement)
+  .duration(1500)
+  .easing('ease-out')
+  .fromTo('transform', 'rotate(0deg)', 'rotate(360deg)');
+
+  simpleAnimation.play();
+  }
 }
